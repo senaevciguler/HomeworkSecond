d42439cd100919e2b9c1d89d568ab2f2803dc16d
senaevciguler <51003682+senaevciguler@users.noreply.github.com>
2020-01-15 11:23:54 +0200

    Create PrintReserve.java

---
+++ PrintReserve.java
@@ -0,0 +1,15 @@
+
+public class PrintReserve {
+	public static void main(String [] args) {
+	
+		int[] arr = {2 , 3, 6, 8, 9};
+		
+		
+		for(int i = 4; i >= 0; i--) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+
+
+}
