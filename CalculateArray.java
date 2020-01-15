4101e461110c3db9c68ba2460f5a026bc3c75a21
senaevciguler <51003682+senaevciguler@users.noreply.github.com>
2020-01-15 11:09:41 +0200

    Create CalculateArray.java

---
+++ CalculateArray.java
@@ -0,0 +1,18 @@
+//write a java program to calculate and print the sum of values of an array.
+
+public class CalculateArray
+ {
+	public static void main (String[] args) {
+	
+	int[] arr = {2,4,6,8};
+	
+	int sum = 0;
+	for(int number:arr) {
+		sum = sum+number;
+
+	}
+	//System.out.println("the sum is: " + sum);
+	}
+
+
+}
